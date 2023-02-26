eb1ac6b57508a27b1a0443d4702655bc16af3454
Sam Ma <samma9137@yahoo.com>
2023-02-26 14:28:30 -0500

    script update

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -7,7 +7,7 @@
 */
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
-const slidePosition = 0;
+let slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
 const slides = document.querySelectorAll('.carousel_item');
@@ -19,6 +19,12 @@ const slidesArray = Array.from(slides);
 const totalSlides = slidesArray.length;
 
 function updateSlidePosition() {
+  slidesArray.forEach((slide) => {
+    slide.classList.remove('visible');
+    slide.classList.add('hidden');
+  });
+
+  slides[slidePosition].classList.add('visible');
   // Using the .forEach array method, (array.forEach((element) => { per-element work goes here }))
   // loop through all the slides in your slideArray
   // and remove the 'visible' class from each classList
@@ -29,6 +35,11 @@ function updateSlidePosition() {
 }
 
 function moveToNextSlide() {
+  if (slidePosition === totalSlides -1){
+    slidePosition = 0;
+  } else{
+    slidePosition +=1;
+  }
   /*
     add an if statement here that checks
     if you're already at the max number of slides
@@ -38,6 +49,11 @@ function moveToNextSlide() {
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
+  if (slidePosition === 0){
+    slidePosition = 3;
+  } else {
+    slidePosition -=1;
+  }
   // add your code in here for when you click the "prev" button
   /*
     add an if statement here that checks
@@ -60,3 +76,8 @@ document.querySelector('.next') // Get the appropriate element (<button class="n
 
 // Paying close attention to the above queryselector, write one that fires
 // when you want a "prev" slide
+document.querySelector('prev')
+  .addEventListener('click', () =>{
+    console.log('clicked prev');
+    moveToPrevSlide();
+  });
