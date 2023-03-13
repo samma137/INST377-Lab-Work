bf29b628ea81927022b4356c274f3cbabfc554a3
Sam Ma <samma9137@yahoo.com>
2023-03-12 20:34:17 -0400

    updates

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -5,6 +5,11 @@
 
 /* A quick filter that will return something based on a matching input */
 function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  })
   /*
     Using the .filter array method, 
     return a list that is filtered by comparing the item name in lower case
@@ -16,6 +21,7 @@ function filterList(list, query) {
 
 async function mainEvent() { // the async keyword means we can make API requests
   const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const filterButton = document.querySelector('.filter_button');
   // Add a querySelector that targets your filter button here
   
   let currentList = []; // this is "scoped" to the main event function
@@ -50,18 +56,17 @@ async function mainEvent() { // the async keyword means we can make API requests
     console.table(currentList); 
   });
 
+filterButton.addEventListener('click', (event) => {
+  console.log('clicked FilterButton');
 
-  /*
-    Now that you HAVE a list loaded, write an event listener set to your filter button
-    it should use the 'new FormData(target-form)' method to read the contents of your main form
-    and the Object.fromEntries() method to convert that data to an object we can work with
+  const formData = new FormData(form);
+  const formProps = Object.fromEntries(formData);
 
-    When you have the contents of the form, use the placeholder at line 7
-    to write a list filter
+  console.log(formProps);
+  const newList = filterList(currentList, formProps.resto);
 
-    Fire it here and filter for the word "pizza"
-    you should get approximately 46 results
-  */
+  console.log(newList);
+})
 }
 
 /*
